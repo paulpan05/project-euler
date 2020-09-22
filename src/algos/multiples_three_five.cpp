19e84483047aad129f6f71f99422e497621267a8
Paul Pan <panjunhong05@gmail.com>
2020-09-22 12:13:07 -0700

    Added function hint.

--- src/algos/multiples_three_five.cpp
+++ src/algos/multiples_three_five.cpp
@@ -4,5 +4,6 @@ int multiples_three_five(int number) {
   int count_3 = (number - 1) / 3;
   int count_5 = (number - 1) / 5;
   int count_15 = (number - 1) / 15;
+  // Hint - use geometric series.
   return (3 * count_3 * (count_3 + 1) / 2) + (5 * count_5 * (count_5 + 1) / 2) - (15 * count_15 * (count_15 + 1) / 2);
 }
